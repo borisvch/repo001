98f145174d13738223123bac58a87af57a7ef348
MLP <marc.lp@icloud.com>
2023-02-20 22:17:36 -0500

    Testing deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+ls
+echo Enter Commit Comment:
+read var_comment
+
+echo =================================================================
+echo Uglifying JS and CSS files...
+uglifyjs video-popup.js > vouchtube-video-popup.min.js
+uglifycss video-popup.css > vouchtube-video-popup.min.css
+echo done.
+echo =================================================================
+echo Pushing to github repository
+git add .
+git commit -m "$var_comment"
+git push 
+echo =================================================================
+echo done.
